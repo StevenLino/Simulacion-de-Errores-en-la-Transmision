5a8af60558507d131d53dbb92df53a2aaf1d3483
Sambek99 <bgaona@espol.edu.ec>
2024-08-11 15:36:30 -0500

    cambios y arreglos

--- funciones.py
+++ funciones.py
@@ -21,10 +21,32 @@ def txt_to_bin(input_txt_path, output_bin_path):
     
     # Convertir el contenido a binario
     binary_data = text_data.encode('utf-8')
+    print(binary_data)
+
+    # Convierte cada byte a su representación binaria
+    binario = ''.join(format(byte, '08b') for byte in binary_data)
+    print(binario)
     
     # Guardar los datos binarios en un nuevo archivo .bin
-    with open(output_bin_path, 'wb') as bin_file:
-        bin_file.write(binary_data)
+    with open(output_bin_path, 'w') as bin_file:
+        bin_file.write(binario)
+
+#Función que estima el peso de un archivo y define en cuantas tramas se dividirá
+def file_size(file_name):
+  with open(file_name, 'r') as data:
+    text_data = data.read()
+    data = [text_data[i:i + 8] for i in range(0, len(text_data), 8)]
+    size = len(data)
+    return size,data
+
+     
+
+        
+
 
 #Función que separa el archivo .bin en segmentos más pequeños para poder simular la segmentación del proceso TCP/IP
+#Se asume que el dispositivo que se tiene solo puede enviar maximo 1024 bits y se tiene que dividir el archivo en la cantidad
+#de segmentos necesarios para satisfacer los requerimientos.
+
+
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Aqui estará el programa primcipal que usará las funciones del archivo def.pv y los documentos en este repositorio
 import funciones as fd
-print("hello word")
-print("xD")
 
 fd.create_txt()
 fd.txt_to_bin("hola.txt","hola.bin")
+d= fd.file_size("hola.bin")
+print(d)
